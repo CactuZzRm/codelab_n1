6ddcc07a08564e3b9001282617ec3d748c5a42a9
cactuzz <926556@mail.ru>
2022-04-22 22:00:56 +0300

    codelab_commit_n1

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:codelab_n1/widgets/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage()
+    )
+  );
+}
---
+++ lib/widgets/mounts_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MountsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to Mounts of the World')
+      )
+    );
+  }
+}
